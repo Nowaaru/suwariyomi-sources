e5e974be2f6afd2cc2bf8965a7ff6d508bdb4c50
Nowaaru <zackyboy135@gmail.com>
2022-03-28 15:36:57 -0700

    test

--- .github/scripts/gen-dist.sh
+++ .github/scripts/gen-dist.sh
@@ -4,7 +4,7 @@ set -e # prevent errors
 SOURCES=(./src/*)
 mkdir -p dist/zip
 
-ls $PWD/dist
+ls ../../
 for SOURCE in ${SOURCES[@]}; do
     echo "Compressing $SOURCE"
     zip -r dist/zip/$(basename $SOURCE).zip $SOURCE
