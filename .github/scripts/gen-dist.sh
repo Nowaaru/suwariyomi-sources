4eff8f339aa9ddd6032f1ce844009f91a19d68ab
Nowaaru <zackyboy135@gmail.com>
2022-03-28 16:47:12 -0700

    cleanup

--- .github/scripts/gen-dist.sh
+++ .github/scripts/gen-dist.sh
@@ -2,15 +2,9 @@
 echo "starting"
 set -e # prevent errors
 
-# check if the "zip" folder exists in the "dist" folder - if it does, then clear it out
-if [ -d "dist/zip" ]; then
-  rm -rf dist/zip
-fi
-
 mkdir -p dist/zip
 SOURCES=(./src/*)
 for SOURCE in ${SOURCES[@]}; do
     echo "Compressing $SOURCE"
     zip -r dist/zip/$(basename $SOURCE).zip $SOURCE
 done
-
