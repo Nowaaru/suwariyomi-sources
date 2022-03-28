e52f03d6a41d1c6cd3ff13a26e222b719e937401
Nowaaru <zackyboy135@gmail.com>
2022-03-28 15:47:41 -0700

    test

--- .github/scripts/gen-dist.sh
+++ .github/scripts/gen-dist.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 set -e # prevent errors
 
-SOURCES=(./src/*)
-mkdir -p dist/zip
+git clone --depth 1 --branch dist
+# check if the "zip" folder exists in the "dist" folder - if it does, then clear it out
+if [ -d "dist/zip" ]; then
+  rm -rf dist/zip
+fi
 
-ls ../../
+cd .. # go back to the root directory
+
+git config --global user.email "github-actions[bot]@users.noreply.github.com"
+git config --global user.name "github-actions[bot]"
+git status
+
+
+
+SOURCES=(./src/*)
 for SOURCE in ${SOURCES[@]}; do
     echo "Compressing $SOURCE"
     zip -r dist/zip/$(basename $SOURCE).zip $SOURCE
 done
 
-
+cd dist
+git add .
+git commit -m "Update"
+git push origin dist
