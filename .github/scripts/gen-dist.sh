803f5daeb4b1a5cf227d9c202e4f6f9c8f011c07
Noire <zackyboy135@gmail.com>
2022-04-11 22:17:34 -0700

    introduce `-j` and more verbosity

--- .github/scripts/gen-dist.sh
+++ .github/scripts/gen-dist.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-echo "starting"
+echo "Beginning compression."
 set -e # prevent errors
 
 mkdir -p dist/zip
 SOURCES=(./src/*)
 for SOURCE in ${SOURCES[@]}; do
     echo "Compressing $SOURCE"
-    zip -r dist/zip/$(basename $SOURCE).zip $SOURCE
+    zip -r -j dist/zip/$(basename $SOURCE).zip $SOURCE
 done
 
+echo "Compressed files:"
 ls dist/zip
