8937ebed32d7b8ecd7be34a9d0f022af51460b97
Nowaaru <zackyboy135@gmail.com>
2022-05-08 00:58:06 -0700

    validation for metadata

--- .github/scripts/gen-json/main.js
+++ .github/scripts/gen-json/main.js
@@ -4,11 +4,55 @@ const fs = require("fs");
 const mainDirectory = path.resolve("./");
 const srcDirectory = path.join(mainDirectory, "src");
 
+const schemaLayout = {
+  displayName: String,
+  zip: String,
+  version: String,
+  nsfw: Boolean,
+  icon: String,
+};
+
 const totalJSON = [];
 fs.readdirSync(srcDirectory).forEach((folder) => {
-  const jsonDirectory = path.join(srcDirectory, folder, "metadata.json");
+  const jsonPath = path.join(srcDirectory, folder, "metadata.json");
   if (fs.existsSync(jsonDirectory)) {
     const metadata = require(jsonDirectory);
+    Object.keys(schemaLayout).forEach((key) => {
+      let isValueIncorrect = false;
+      switch (schemaLayout[key]) {
+        case String:
+          if (typeof metadata[key] !== "string") {
+            isValueIncorrect = true;
+          }
+          break;
+        case Boolean:
+          if (typeof metadata[key] !== "boolean") {
+            isValueIncorrect = true;
+          }
+          break;
+        case Number:
+          if (typeof metadata[key] !== "number") {
+            isValueIncorrect = true;
+          }
+          break;
+        default:
+          console.warn("WARNING! Metadata has an unknown key:", key);
+          return delete metadata[key];
+      }
+
+      if (isValueIncorrect) {
+        console.warn(
+          "WARNING! Metadata has an invalid value for key:",
+          key,
+          "expected",
+          schemaLayout[key],
+          "got",
+          typeof metadata[key]
+        );
+
+        return delete metadata[key];
+      }
+    });
     totalJSON.push(metadata);
   }
 });
